da86ea0e305024015b6662920a8c4d320fcaa7f9
hikkamuru <hikkamuru@gmail.com>
2026-03-25 13:59:28 +0300

    Add files via upload

--- diary_bot/config.py
+++ diary_bot/config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 BOT_TOKEN = "6363554934:AAEeMVjOZBuCaHZAc5DuBGF9OAJzuFojC3A"
 
-YANDEX_API_KEY = "AQVNy1b2NII6OxVUu3fRU5SXp9UzUrynnW7FbK9c"
+YANDEX_API_KEY = "AQVN1Q1vzGP8u8XlLwTEO-E10CMpQ3wE6p79-Aiq"
 
 BASE_DIR = Path(__file__).parent
 DB_PATH = BASE_DIR / "diary.db"
